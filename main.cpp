065bd0b9de9b14ccbfba57520af5f3c3568cdcc4
Boda-Peter <boda.peter14@gmail.com>
2022-09-19 20:11:22 +0200

    A hibák kijavítva

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //Hiányzik az alulvonás
-    std::cout << '1-100 ertekek duplazasa'  //Rossz aposztróf és hiányzik a pontosvessző
-    for (int i = 0;)        //Hiányzik a léptetés a ciklusból
+    int *b = new int[N_ELEMENTS]; //Hiányzik az alulvonás
+    std::cout << "1-100 ertekek duplazasa" << std::endl;  //Rossz aposztróf és hiányzik a pontosvessző
+    for (int i = 0; i <N_ELEMENTS ; i++)        //Hiányzik a léptetés a ciklusból
     {
-        b[i] = i * 2;       //1-től 100-ig kéne de 0-tól 99-ig történik csak meg a duplázás
+        b[i] = (i+1) * 2;       //1-től 100-ig kéne de 0-tól 99-ig történik csak meg a duplázás
     }
-    for (int i = 0; i; i++)     //Hibás ciklusvezérlő mindig hamisra fut le
+    for (int i = 0; i<N_ELEMENTS; i++)     //Hibás ciklusvezérlő mindig hamisra fut le
     {
-        std::cout << "Ertek:"       //Maga az érték nincs kiiratva
+        std::cout << "Ertek:" <<b[i] <<std:: endl;      //Maga az érték nincs kiiratva
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;              //változónak nincs kezdőértéke
-    for (int i = 0; i < N_ELEMENTS, i++)         //Hiányzik egy pontosvessző
+    int atlag = 0;              //változónak nincs kezdőértéke
+    for (int i = 0; i < N_ELEMENTS; i++)         //Hiányzik egy pontosvessző
     {
-        atlag += b[i]           //Hiányzik a pontosvessző
+        atlag += b[i];           //Hiányzik a pontosvessző
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
